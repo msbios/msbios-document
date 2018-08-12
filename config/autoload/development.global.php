97b0eb0366bf40e538f120768da01ac03df36d24
Judzhin Miles <info@msbios.com>
2018-08-12 22:57:55 +0300

    INtermedaite commit

--- config/autoload/development.global.php
+++ config/autoload/development.global.php
@@ -4,9 +4,16 @@
  * @author Judzhin Miles <info[woof-woof]msbios.com>
  */
 return [
+
+    'db' => [
+        'dsn' => 'mysql:dbname=portal.dev;host=127.0.0.1',
+        'username' => 'root',
+        'password' => 'root',
+    ],
+
     \MSBios\Theme\Module::class => [
-//        'default_global_paths' => [
-//            'default_global_paths' => __DIR__ . '/../../themes/'
-//        ]
+        // 'default_global_paths' => [
+        //     'default_global_paths' => __DIR__ . '/../../themes/'
+        // ]
     ]
 ];
